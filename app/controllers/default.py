07bfbd428cfe93fee67ef2fe898423cc272b6342
Mateus Varelo <mateusferreira703@gmail.com>
2021-08-17 13:24:06 -0300

    fazendo rotas

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,14 @@
 from app import app
 
-
+@app.route("/index", methods = ['GET'])
 @app.route("/")
 def index():
     return "Hello word!"
+
+@app.route("/teste",defaults = {'nome': None})
+@app.route("/teste/<nome>")
+def teste(nome):
+    if nome:
+       return f"Oi Sr.{nome}"
+    else:
+       return f"Oi, sem nome" 
