6c85428b27b151e030ce78d8ee71af64ff1ac80d
Mateus Varelo <mateusferreira703@gmail.com>
2021-08-17 20:57:56 -0300

    deploy

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,10 +1,11 @@
 from app import app
+from flask import render_template
 
 @app.route("/index", methods = ['GET'])
 @app.route("/")
 def index():
-    return "Hello word!"
+    return render_template('index.html')
 
-@app.route("/graficos")
+@app.route("/graficos_1")
 def grafico():
     return "Parte do grafico"
